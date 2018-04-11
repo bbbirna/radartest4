8b59be218c65611d88476919930f47fc3a735f0e
Birna Birna <birnabirna@Birnas-MBP-2.tskoli.is>
2018-04-11 10:25:33 +0000

    new key

--- src/App.js
+++ src/App.js
@@ -54,7 +54,8 @@ class App extends Component {
           lat = {this.state.lat}
           lng = {this.state.lng}
           isMarkerShown
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          // googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTcaCpwGdxHbSxX6fxzy_bzqqy6mVK31Q"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />} />
